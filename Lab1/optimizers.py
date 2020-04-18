2d7a4c014e5e641ad1fcd441fdb6d53da30a3cdc
Dmitry Belikov <belikoff@yandex-team.ru>
2020-04-18 06:34:37 +0300

    newton backend for regression + newton/gradient comparision

--- Lab1/optimizers.py
+++ Lab1/optimizers.py
@@ -4,24 +4,34 @@ from Lab1.gradient_descent import gradient_descent, stop_criterion_dx
 
 
 class Optimizer:
+    def __init__(self):
+        self.iters = 0
+
     @abstractmethod
     def minimize(self, x):
         raise NotImplemented
 
+    def get_iters(self):
+        return self.iters
+
 
 class Newton(Optimizer):
     def __init__(self, func, grad, hess, eps=1e-9):
+        super(Newton, self).__init__()
         self.func = func
         self.grad = grad
         self.hess = hess
         self.eps = eps
 
     def minimize(self, x):
-        return newton(self.func, self.grad, self.hess, x, self.eps, None)[0]
+        result, iters, _ = newton(self.func, self.grad, self.hess, x, self.eps, None)
+        self.iters += iters
+        return result
 
 
 class GradientDescent(Optimizer):
     def __init__(self, func, grad, eps=1e-9, searcher=None, stop_criterion=stop_criterion_dx):
+        super(GradientDescent, self).__init__()
         self.func = func
         self.grad = grad
         self.eps = eps
@@ -29,4 +39,6 @@ class GradientDescent(Optimizer):
         self.stop_criterion = stop_criterion
 
     def minimize(self, x):
-        return gradient_descent(self.func, self.grad, x, self.eps, self.searcher, self.stop_criterion)[0]
+        result, iters, _ = gradient_descent(self.func, self.grad, x, self.eps, self.searcher, self.stop_criterion)
+        self.iters += iters
+        return result
--- Lab1/regression/regression.py
+++ Lab1/regression/regression.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 import scipy.special
 import matplotlib.pyplot as plt
-from Lab1.optimizers import GradientDescent
+import time
+from Lab1.optimizers import GradientDescent, Newton
 from Lab1.methods import GoldenRatioSearcher
 
 
@@ -17,6 +18,19 @@ def regression_gradient(X, y, w, reg):
     return X.T.dot(-y * exp_division).T + reg * w
 
 
+def regression_hessian(X, y, w, reg):
+    m, n = X.shape
+    result = np.zeros((n, n))
+    for k in range(n):
+        for j in range(n):
+            for i in range(m):
+                exp_part = np.exp(-y[i] * np.inner(X[i], w))
+                special_part = scipy.special.expit(-y[i] * np.inner(X[i], w))
+                result[k, j] += y[i] ** 2 * X[i, k] * X[i, j] * special_part / exp_part
+    result += reg * np.eye(n)
+    return result
+
+
 def minimization_function(X, y, reg):
     return lambda w: regression_function(X, y, w, reg)
 
@@ -25,28 +39,62 @@ def minimization_gradient(X, y, reg):
     return lambda w: regression_gradient(X, y, w, reg)
 
 
+def minimization_hessian(X, y, reg):
+    return lambda w: regression_hessian(X, y, w, reg)
+
+
+def process_task(optimizer, w0):
+    start_time = time.time()
+    weights = optimizer.minimize(w0)
+    iterations = optimizer.get_iters()
+    end_time = time.time()
+    task_time = end_time - start_time
+    return {
+        'weights': weights,
+        'iters': iterations,
+        'time': task_time
+    }
+
+
 def logistic_regression(X, y):
     X = np.hstack((np.ones((X.shape[0], 1)), X))
-    reg = 0
+    reg = 0.5
     min_func = minimization_function(X, y, reg)
     min_grad = minimization_gradient(X, y, reg)
-    optimizer = GradientDescent(min_func, min_grad, searcher=GoldenRatioSearcher)
+    min_hess = minimization_hessian(X, y, reg)
+    optimizer_descent = GradientDescent(min_func, min_grad, eps=1e-15, searcher=GoldenRatioSearcher)
+    optimizer_newton = Newton(min_func, min_grad, min_hess, eps=1e-15)
     w0 = np.array([0] * X.shape[1], dtype=np.float)
-    weights = optimizer.minimize(w0)
-    return weights
+    descent_result = process_task(optimizer_descent, w0)
+    newton_result = process_task(optimizer_newton, w0)
+    return descent_result, newton_result
+
+
+def get_points(weights):
+    xs = [0, 25]
+    ys = [-weights[0] / weights[2], (-weights[0] - weights[1] * 25) / weights[2]]
+    return xs, ys
 
 
 def train_regression(dataset_path):
     df = pd.read_csv(dataset_path)
     X = df.drop(['binaryClass'], axis=1).values
     y = df.binaryClass.map(lambda c: -1 if c == 'N' else 1).values
-    weights = logistic_regression(X, y)
-    print(weights)
-    xs = [0, 25]
-    ys = [-weights[0] / weights[2], (-weights[0] - weights[1] * 25) / weights[2]]
-    plt.plot(xs, ys, color='green')
+    descent_result, newton_result = logistic_regression(X, y)
+
+    weights_descent = descent_result['weights']
+    weights_newton = newton_result['weights']
+
+    xs_descent, ys_descent = get_points(weights_descent)
+    xs_newton, ys_newton = get_points(weights_newton)
+
+    plt.plot(xs_descent, ys_descent, color='green')
+    plt.plot(xs_newton, ys_newton, color='violet')
     plt.scatter(df.col_1, df.col_2, color=df.binaryClass.map(lambda c: 'red' if c == 'P' else 'blue'))
     plt.show()
+
+    print('Newton took {} iterations and {} seconds to complete'.format(newton_result['iters'], newton_result['time']))
+    print('Gradient descent took {} iteration and {} seconds to complete'.format(descent_result['iters'], descent_result['time']))
 
 
 if __name__ == '__main__':
